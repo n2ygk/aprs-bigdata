0ef90b7ae18ccc47f5257b33139d82b38632db21
Alan Crosswell <alan@crosswell.us>
2013-05-30 22:54:07 -0400

    Completed course/speed calculation, mic_e, pos_cmp Fixed firsthop calculation to deal with WIDE2-1 (for example)

--- aprspig.py
+++ aprspig.py
@@ -42,10 +42,15 @@ def aprs(l):
   s = re.match('^(?P<time>^[^ ]*) (?P<from_call>[^>]+)>(?P<to_call>[^,]+),*(?P<digis>.*),(?P<gtype>[^,]+),(?P<gate>[^:]+)(:)(?P<info>.*)$',l)
   if s:
     digis = s.group('digis')
+    d1 = digis.split(',')[0]
     if '*' in digis:            # if there's a repeated flag set in digis, then the first hop is here.
-      firsthop=digis.split(',')[0].rstrip('*')
-    else:                       # if not, then the first hop is the IGATE
-      firsthop=s.group('gate')
+      firsthop=d1.rstrip('*')
+    else:                       # if not, then the first hop is the may be a hidden flood call or IGATE
+      r = re.match('^\D+(?P<N>\d?)-?(?P<n>\d?)$',d1)
+      if r and r.group('N')!=r.group('n'): # a floodN-n digi that has been used (decremented)
+        firsthop = d1
+      else:
+        firsthop=s.group('gate')
     return s.group('time'),s.group('from_call'),s.group('to_call'),s.group('digis'),s.group('gtype'),s.group('gate'),s.group('info'),firsthop
 
 class Position:
@@ -55,6 +60,16 @@ class Position:
   """
   def __init__(self):
     self.lat = self.lon = self.lat_a = self.lon_a = self.cse = self.spd = 0.0
+    self.ambiguity = {0: 0.0,        # no ambiguity
+                      1: 0.1/60.0,   # 4903.5 N represents latitude to nearest 1/10th of a minute. 
+                      2: 1.0/60.0,   # 4903.  N represents latitude to nearest minute.
+                      3: 10.0/60.0,  # 490 .  N represents latitude to nearest 10 minutes. 
+                      4: 1.0,        # 49  .  N represents latitude to nearest degree.
+                      5: 10.0,       # not an actual valid value!
+                      6: 100.0,      # ditto
+                      7: 1000.0,     # make it clear this is just stupid!
+                      }
+
 
   def lat2DD(self,lat,NS):
     """ convert degrees, minutes, NS compass direction to decimal degrees """
@@ -65,23 +80,18 @@ class Position:
     """ convert degrees, minutes, EW compass direction to decimal degrees """
     self.lon = float(lon[0:3]) + float(lon[3:])/60.0
     if EW == 'W': self.lon = -self.lon
-    
+  
   def pos(self,to_call,info):
     """
     Source: APRS101.pdf
     Section 7: Normal position reports:
    
     4903.50N/07201.75W-  where the / and - can be a variety of symbols. Special case \. means null position.
-    with position ambiguity spaces in place of digits.
-    4903.5 N represents latitude to nearest 1/10th of a minute. 
-    4903.  N represents latitude to nearest minute.
-    490 .  N represents latitude to nearest 10 minutes. 
-    49  .  N represents latitude to nearest degree.
     
     optionally followed by course & speed (CSE/SPD) (and several other extensions)
     ddd/sss (e.g. 088/036)
    
-    TheNet X-1J4... header is obsolete and can be skipped.
+    TheNet X-1J4... header may be obsolete and can be skipped.
     [......] Maidenhead grid squares: skip this for now
    
     Section 8:
@@ -90,29 +100,50 @@ class Position:
     !4903.50N/07201.75W-comment text /A=001234
     =4903.50N/07201.75W-comment text /A=001234
     """
+    if re.match('\D',info[1]):  # if first char of lat is not a digit this is a compressed position report
+      return self.pos_cmp(to_call,info)
     if info[9]=='\\' and info[19]=='.': return None
     # "The level of ambiguity specified in the latitude will automatically apply to the longitude as well...."
-    ambiguity = {0: 0.0,        # no ambiguity
-                 1: 0.1/60.0,   # 4903.5 N represents latitude to nearest 1/10th of a minute. 
-                 2: 1.0/60.0,   # 4903.  N represents latitude to nearest minute.
-                 3: 10.0/60.0,  # 490 .  N represents latitude to nearest 10 minutes. 
-                 4: 1.0,        # 49  .  N represents latitude to nearest degree.
-                 5: 10.0,       # not an actual valid value!
-                 6: 100.0,      # ditto
-                 7: 1000.0,     # make it clear this is just stupid!
-                 }
     _lat = info[1:8]
     _NS = info[8]
     _lon = info[10:18]
     _EW = info[18]
-    self.lat_a = self.lon_a = ambiguity[_lat.count(' ')]
+    _csespd = info[20:]
+    self.lat_a = self.lon_a = self.ambiguity[_lat.count(' ')]
     if self.lat_a > 0.0:
       _lat = _lat.replace(' ','0')
       _lon = _lon.replace(' ','0')
     # DDMM.mm -> DD + MM.mm/60
     self.lat2DD(_lat,_NS)
     self.lon2DD(_lon,_EW)
-    # TODO: CSE/SPD
+    # CSE/SPD
+    r = re.match('^(?P<cse>\d\d\d)/(?P<spd>\d\d\d)',_csespd)
+    if r:
+      self.cse = float(r.group('cse'))
+      self.spd = float(r.group('spd'))
+    return self.value()
+
+  def pos_cmp(self,to_call,info):
+    """
+    Section 9:
+    Compressed position reports
+    !/YYYYXXXX$csT - Ugh, just read pages 37-41
+    01234567890123
+    """
+    _lat = info[2:6]
+    _lon = info[6:10]
+    _c = info[10]
+    _s = info[11]
+    _type = info[12]
+
+    self.lat = 90.0 - float((ord(_lat[0])-33) * 91**3 + (ord(_lat[1])-33) * 91**2 + (ord(_lat[2])-33) * 91 + ord(_lat[3])-33)/380926.0
+    self.lon = -180.0 + float((ord(_lon[0])-33) * 91**3 + (ord(_lon[1])-33) * 91**2 + (ord(_lon[2])-33) * 91 + ord(_lon[3])-33)/190463.0
+    if _c == ' ' or _c == '{':
+      return self.value()
+    c = ord(_c) - 33
+    s = ord(_s) - 33
+    self.cse = c * 4.0
+    self.spd = 1.08 ** s - 1.0
     return self.value()
 
   def pos_ts(self,to_call,info):
@@ -124,14 +155,6 @@ class Position:
     @092345/4903.50N/07201.75W-comment text /A=001234
     """
     return self.pos(to_call,info[7:])
-
-  def pos_cmp(self,to_call,info):
-    """
-    Section 9:
-    Compressed position reports
-    /YYYYXXXX$csT - Ugh, just read pages 37-41
-    """
-    pass
 
   def gps(self,to_call,info):
     """
@@ -164,12 +187,51 @@ class Position:
     self.lon2DD(_lon,_EW)
     return self.value()
 
-  def mic_e(self,to_call,info):
+  def mic_e(self,t,i):
     """
-    Section 10:
+    parse position information from a Mic-E packet. 
+    ignores message bits, telemetry, etc.
+    See Section 10:
     Mic-E pages 42-56
     """
-    pass
+    beta = i[0] in '\0x1c\0x1d'
+    # check for position ambiguity (not sure this is correct)
+    self.lat_a = self.lon_a = self.ambiguity[t[0:6].count('Z')]
+    # latitude HHMM.SS are encoded as right nibbles of to_call.
+    latDD = (ord(t[0])&0x0f)*10 + (ord(t[1])&0x0f)
+    latMM = ((ord(t[3])&0x0f)*10.0+ (ord(t[3])&0x0f))/60.0
+    latHH = ((ord(t[4])&0x0f)*.1 + (ord(t[5])&0x0f)*.01)/60.0
+    # north and west (and other flag) bits are in the left nibbles.
+    north = ord(t[3])&0x40
+    west = ord(t[5])&0x40
+    self.lat = float(latDD) + latMM + latHH
+    if not north: self.lat = -self.lat 
+    lon_offset = ord(t[4])&0x40
+    lonDD = ord(i[1]) - 28;
+    lonMM = float(ord(i[2]) - 28)/(60.0);
+    lonHH = float((ord(i[3]) - 28)*.01)/(60.0);
+    if not beta:                # this stuff is not clearly documented
+      if lon_offset:
+        lonDD += 100;
+      if 180 <= lonDD and lonDD <= 189:
+        lonDD -= 80;
+      if 190 <= lonDD and lonDD <= 199:
+        lonDD -= 190;
+      if lonMM >= 60:
+        lonMM -= 60;
+    self.lon = float(lonDD) + lonMM + lonHH
+    if west: self.lon = -self.lon
+    sp = ord(i[4]) - 28;
+    dc = ord(i[5]) - 28;
+    se = ord(i[6]) - 28;
+    self.spd = (sp*10.0)+(dc/10.0);
+    self.cse = (dc%10)*100.0+se;
+    if not beta:
+      if self.spd >= 800:
+	self.spd -= 800;
+      if self.cse >= 400:
+	self.cse -= 400;
+    return self.value()
 
   def parse(self,to_call,info):
     """
